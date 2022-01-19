95502e8a8456b0d51ca7bf188e109606e5029e36
ummerukaya <ummerukaya13@gmail.com>
2022-01-19 16:40:19 +0600

    added a file in the test branch

---
+++ src/Presentation/Nop.Web/GitBranchTestingPurposeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web
+{
+    public class GitBranchTestingPurposeClass
+    {
+        public void Test()
+        {
+            Console.WriteLine("Added in test branch");
+        }
+    }
+}
